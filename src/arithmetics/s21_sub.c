75a010c988daa065e5f01b0cd1bdb562ec498d5d
Ryo Vamirion <rynortheast@yandex.ru>
2022-09-17 18:56:58 +0300

    Зафиксировано несколько ошибок тестов + исправлен s21_nagate

--- src/arithmetics/s21_sub.c
+++ src/arithmetics/s21_sub.c
@@ -2,6 +2,7 @@
 
 // TODO [s21_sub] Он прошел вертер?
 // TODO [s21_sub] Так и не понял как работает convert.
+// TODO [s21_sub] ОШИБКА В ТЕСТЕ. СМ. ВНИЗУ ФАЙЛА.
 // ПРИМЕР. 3 - 2 = 1. Единственный способ реализовать это через + это выражение -2 + 3,
 // НО! функции s21_add выполняет подобные выражения через s21_sub, то есть выходит цикл, кек.
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal * result) {
@@ -31,6 +32,39 @@ int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal * result) {
       }
     }
   }
-  // if (res && result_sign) res = 2;
+  // if (status && result_sign) res = 2;
   return status;
 }
+
+// START_TEST(sub_11) {
+//   s21_decimal val1 = {
+//       {UINT_MAX, UINT_MAX, UINT_MAX, ~(UINT_MAX / 2)}};
+//   s21_decimal val2 = {{4, 0, 0, 0}};
+//   s21_decimal res;
+//   ck_assert_int_eq(2, s21_sub(val1, val2, &res));
+// }
+// END_TEST
+
+// START_TEST(sub_14) {
+//   s21_decimal val1 = {{4, 0, 0, ~(UINT_MAX / 2)}};
+//   s21_decimal val2 = {{UINT_MAX, UINT_MAX, UINT_MAX, 0}};
+//   s21_decimal res;
+//   ck_assert_int_eq(2, s21_sub(val1, val2, &res));
+// }
+// END_TEST
+
+// START_TEST(sub_20) {
+//   s21_decimal dec1;
+//   s21_decimal dec2;
+//   int tmp1 = 100;
+//   int tmp2 = 99999;
+//   int res_s21 = 0;
+//   int res = -99899;
+//   s21_decimal res1;
+//   s21_from_int_to_decimal(tmp1, &dec1);
+//   s21_from_int_to_decimal(tmp2, &dec2);
+//   s21_sub(dec1, dec2, &res1);
+//   s21_from_decimal_to_int(res1, &res_s21);
+//   ck_assert_int_eq(res_s21, res);
+// }
+// END_TEST
--- src/other/s21_negate.c
+++ src/other/s21_negate.c
@@ -1,10 +1,11 @@
 #include "./../s21_decimal.h"
 
+// TODO [s21_negate] В ELSE почему-то стоял setSign(result, >>> -1 <<<)
 int s21_negate(s21_decimal value, s21_decimal * result) {
     s21_copy(result, value);
     if (getSign(*result))
         setSign(result, 1);
     else
-        setSign(result, -1);
+        setSign(result, 0);
     return 0;
 }
