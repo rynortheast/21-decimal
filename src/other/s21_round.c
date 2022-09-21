cb1fd48d7a165eef1226091d2c2c3c174e00b6cc
Ryo Vamirion <rynortheast@yandex.ru>
2022-09-21 13:34:43 +0300

    Исправлена ошибка в s21_round - неправильное округление
    - Функция полностью переписана.

--- src/other/s21_round.c
+++ src/other/s21_round.c
@@ -1,16 +1,12 @@
 #include "./../s21_decimal.h"
 
 int s21_round(s21_decimal value, s21_decimal* result) {
-  if (!s21_truncate(value, result) && getScale(value)) {
-    s21_decimal remains = {{0, 0, 0, 0}};
-    s21_sub(value, *result, &remains);
-    s21_truncate(*setScale(&remains, getScale(remains) - 1), &remains);
-    if (s21_is_greater_or_equal(remains, ((s21_decimal){{5, 0, 0, 0}}))) {
-      if (getSign(value))
-        s21_sub(*result, ((s21_decimal){{1, 0, 0, 0}}), result);
-      else
-        s21_add(*result, ((s21_decimal){{1, 0, 0, 0}}), result);
-    }
+  if (!s21_truncate(value, result) && !s21_is_equal(value, *result)) {
+    if (getSign(value))
+      s21_sub(value, ((s21_decimal) {{5, 0, 0, 65536}}), &value);
+    else
+      s21_add(value, ((s21_decimal) {{5, 0, 0, 65536}}), &value);
+    s21_truncate(value, result);
   }
   return 0;
 }
