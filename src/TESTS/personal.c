081a456d6fc3770e491fc6b73a8fbba649b1f703
Ryo Vamirion <rynortheast@yandex.ru>
2022-09-21 11:38:52 +0300

    Рефакторинг проекта, внесение небольших правок
    - Удалено бессмысленное комментарование;
    - Исправлен пеерсональный тест;
    - Дополнен Makefile.

--- src/TESTS/personal.c
+++ src/TESTS/personal.c
@@ -1,46 +1,15 @@
 #include "./../s21_decimal.h"
 
-void TEST_func_scaleAlignment();
-
 int main() {
-  TEST_func_scaleAlignment();
+
+  s21_decimal TEST = {{1680878458, 3164, 0, 393216}};
+  s21_decimal ITOG = {{0, 0, 0, 0}};
+
+  printf("TEST: [%d | %d | %d | %d]\n", TEST.bits[0], TEST.bits[1], TEST.bits[2], TEST.bits[3]);
+
+  s21_truncate(TEST, &ITOG);
+
+  printf("ITOG: [%d | %d | %d | %d]\n", ITOG.bits[0], ITOG.bits[1], ITOG.bits[2], ITOG.bits[3]);
 
   return 0;
-}
-
-void TEST_func_scaleAlignment() {
-  // s21_decimal TEST_1 = {{5, 0, 0, 0}};
-  // s21_decimal TEST_2 = {{5, 0, 0, 0}};
-
-  // setSign(&TEST_1, 0);
-  // setSign(&TEST_2, 1);
-
-  s21_decimal TEST_1 = {{5, 0, 0, 0}};
-  // s21_decimal TEST_2 = {{2, 0, 0, 0}};
-  s21_decimal res;
-
-  printf("TEST: %u | %u | %u | %u --- %d\n", TEST_1.bits[0], TEST_1.bits[1],
-         TEST_1.bits[2], TEST_1.bits[3], getSign(TEST_1));
-  // printf("TEST: %u | %u | %u | %u --- %d\n\n", TEST_2.bits[0],
-  // TEST_2.bits[1], TEST_2.bits[2], TEST_2.bits[3], getSign(TEST_2));
-
-  int status = s21_sub(TEST_1, res);
-
-  printf("TEST: %u | %u | %u | %u = %d\n\n", res.bits[0], res.bits[1],
-         res.bits[2], res.bits[3], status);
-
-  // setBit(&TEST_1, 94, 1);
-  // setBit(&TEST_2, 94, 1);
-
-  // setScale(&TEST_2, 2);
-
-  // printf("TEST: %d | %d | %u | %u\n", TEST_1.bits[0], TEST_1.bits[1],
-  // TEST_1.bits[2], TEST_1.bits[3]); printf("TEST: %d | %d | %u | %u\n\n",
-  // TEST_2.bits[0], TEST_2.bits[1], TEST_2.bits[2], TEST_2.bits[3]);
-
-  // alignmentScale(&TEST_1, &TEST_2);
-
-  // printf("TEST: %d | %d | %u | %u\n", TEST_1.bits[0], TEST_1.bits[1],
-  // TEST_1.bits[2], TEST_1.bits[3]); printf("TEST: %d | %d | %u | %u\n\n",
-  // TEST_2.bits[0], TEST_2.bits[1], TEST_2.bits[2], TEST_2.bits[3]);
 }
