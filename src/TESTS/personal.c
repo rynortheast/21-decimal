d96eb417dafa54dac501b91a7295632962153f60
Ryo Vamirion <rynortheast@yandex.ru>
2022-09-21 12:37:51 +0300

    Исправлена функция s21_floor - ошибка в округления
    - При ровном отрицательном значение со scale происходило
    округление в меньшую сторону (5.0 => 6).

--- src/TESTS/personal.c
+++ src/TESTS/personal.c
@@ -2,14 +2,14 @@
 
 int main() {
 
-  s21_decimal TEST = {{1680878458, 3164, 0, 393216}};
+  s21_decimal TEST = {{5050, 0, 0, -2147418112}};
   s21_decimal ITOG = {{0, 0, 0, 0}};
 
-  printf("TEST: [%d | %d | %d | %d]\n", TEST.bits[0], TEST.bits[1], TEST.bits[2], TEST.bits[3]);
+  printf("TEST: [%d | %d | %d | %d] = [scale = %d]\n", TEST.bits[0], TEST.bits[1], TEST.bits[2], TEST.bits[3], getScale(TEST));
 
-  s21_truncate(TEST, &ITOG);
+  s21_floor(TEST, &ITOG);
 
-  printf("ITOG: [%d | %d | %d | %d]\n", ITOG.bits[0], ITOG.bits[1], ITOG.bits[2], ITOG.bits[3]);
+  printf("ITOG: [%d | %d | %d | %d] = [scale = %d]\n", ITOG.bits[0], ITOG.bits[1], ITOG.bits[2], ITOG.bits[3], getScale(TEST));
 
   return 0;
 }
